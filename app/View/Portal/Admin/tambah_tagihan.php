e4fe4fa6843b2e93927c37ae1221d0ff9aaa9c02
Irvan Aulia Luthfi <zeventier@gmail.com>
2022-07-04 23:57:20 +0800

    tambah tagihan dan edit tagihan auto sum fix

--- app/View/Portal/Admin/tambah_tagihan.php
+++ app/View/Portal/Admin/tambah_tagihan.php
@@ -59,7 +59,12 @@
                     </div>
                     <div class="item">
                         <p>Biaya Sewa Rusun/bulan</p>
-                        <input type="number" name="sewa_rusun" required />
+                        <select name="sewa_rusun" required>
+                            <option value="">Select</option>
+                            <option value="300000" >300000</option>
+                            <option value="450000" >450000</option>
+                            <option value="550000" >550000</option>
+                        </select>
                     </div>
                     <div class="item">
                         <p>Total Tagihan</p>
